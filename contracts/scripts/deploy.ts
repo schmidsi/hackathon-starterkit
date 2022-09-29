4e9af5ccadafb73ab8a15def25c69e852ed9bae5
Simon Emanuel Schmid <simon@schmid.io>
2022-09-29 14:37:03 +0200

    Use simple NFT as example

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,20 +1,14 @@
 import hre, { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const MyNFT = await ethers.getContractFactory("MyNFT");
+  const myNFT = await MyNFT.deploy();
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  await myNFT.deployed();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  console.log(`MyNFT deployed to ${myNFT.address}`);
 
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
-
-  await hre.run("graph", { contractName: "Lock", address: lock.address });
+  await hre.run("graph", { contractName: "MyNFT", address: myNFT.address });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
