cdece8d5b3a5045a2c4f6e17c5a0bf49e75cd666
Simon Emanuel Schmid <simon@schmid.io>
2022-10-20 03:46:48 +0200

    Adding Apollo Client and use it for queries

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,17 @@
 import type { AppProps } from 'next/app';
 import { NextUIProvider, globalCss } from '@nextui-org/react';
+import { ApolloProvider } from '@apollo/client';
+
+import { useApollo } from '../lib/apolloClient';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps);
+
   return (
     <NextUIProvider>
-      <Component {...pageProps} />
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </NextUIProvider>
   );
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,29 @@
 import type { NextPage } from 'next';
 import { Button, Col, Container, Row } from '@nextui-org/react';
 
+import { gql, useQuery, NetworkStatus } from '@apollo/client';
+
 const Home: NextPage = () => {
+  const { loading, error, data } = useQuery(
+    gql`
+      query MyQuery {
+        transfers {
+          id
+          from
+          to
+          tokenId
+        }
+        _meta {
+          block {
+            number
+          }
+        }
+      }
+    `,
+  );
+
+  console.log(loading, error, data);
+
   return (
     <Container fluid>
       <Row justify="center" align="center">
