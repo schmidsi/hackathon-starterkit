ecf6e55e3a875944d615809e8e5011179fe34de3
Simon Emanuel Schmid <simon@schmid.io>
2022-10-22 17:22:39 +0200

    Remove Apollo Client

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,17 +1,10 @@
 import type { AppProps } from 'next/app';
 import { NextUIProvider, globalCss } from '@nextui-org/react';
-import { ApolloProvider } from '@apollo/client';
-
-import { useApollo } from '../lib/apolloClient';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const apolloClient = useApollo(pageProps);
-
   return (
     <NextUIProvider>
-      <ApolloProvider client={apolloClient}>
-        <Component {...pageProps} />
-      </ApolloProvider>
+      <Component {...pageProps} />
     </NextUIProvider>
   );
 }
