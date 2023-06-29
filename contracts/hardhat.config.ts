3a9152bcb728c9b06ea8bb94e53fcbff82c975a0
Simon Emanuel Schmid <simon@schmid.io>
2023-06-29 14:46:46 +0200

    Remove temp _contracts again

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,10 +1,20 @@
-import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import '@graphprotocol/hardhat-graph';
 
 import './tasks/deploy';
+import './tasks/mint';
+import './tasks/balanceOf';
+import './tasks/fund';
 
-const config: HardhatUserConfig = {
-  solidity: '0.8.17',
+const config = {
+  solidity: '0.8.9',
+  subgraph: {
+    name: 'mynft',
+    indexEvents: true,
+  },
+  paths: {
+    subgraph: '../subgraph',
+  },
 };
 
 export default config;
